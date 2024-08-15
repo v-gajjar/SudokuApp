8b7bef9cfbfcbef1fb1194173a07a34a92b38c94
JoleneKearse <pharaohnutz@gmail.com>
2024-08-15 11:44:08 -0400

    fixes #1: install tailwind with plugin and set up base colors for body tag

--- main.js
+++
@@ -1,7 +0,0 @@
-import "./style.css";
-
-document.querySelector("#app").innerHTML = `
-  <div>
-    <h1>Sudo App!</h1>
-  </div>
-`;
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./main.js"],
+  theme: {
+    colors: {
+      brand: {
+        50: "#EBFAFA",
+        100: "#D7F4F4",
+        200: "#B2EBEB",
+        300: "#8AE0E0",
+        400: "#66D6D6",
+        500: "#3ECCCC",
+        600: "#2DA9A9",
+        700: "#217D7D",
+        800: "#165555",
+        900: "#0B2828",
+        950: "#051414",
+      },
+      white: "#FFFFFF",
+      error: "#FF8749",
+    },
+    extend: {},
+  },
+  plugins: ["prettier-plugin-tailwindcss"],
+};

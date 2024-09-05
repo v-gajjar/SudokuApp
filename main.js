0ceaab850ecd0bf80054c87a27452bd3722ee375
JoleneKearse <pharaohnutz@gmail.com>
2024-09-05 17:31:38 -0400

    chore: set up responsive gameBoard dimensions

---
+++ main.js
@@ -0,0 +1,4 @@
+// Set height of gameBoard
+const gameBoard = document.getElementById("gameBoard");
+const gameBoardWidth = window.getComputedStyle(gameBoard).width;
+gameBoard.style.height = gameBoardWidth;

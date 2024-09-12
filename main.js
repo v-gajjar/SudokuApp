2035e04126a12acc35740c3f4929687a927f9097
vgajjar <vgajjar22@gmail.com>
2024-09-12 15:46:45 +0100

    set up prototype gameboard using JavaScript

--- main.js
+++ main.js
@@ -1,4 +1,42 @@
+"use strict";
+
 // Set height of gameBoard
 const gameBoard = document.getElementById("gameBoard");
 const gameBoardWidth = window.getComputedStyle(gameBoard).width;
 gameBoard.style.height = gameBoardWidth;
+
+const practiseBoard = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+];
+    
+function generateGameBoard() {
+  for (let i = 0; i < practiseBoard.length; i++) {
+
+    let outerCell = document.createElement("div");
+    outerCell.classList.add(["outerCell"]);
+    outerCell.setAttribute(`id`,`cell-${i}` ); 
+
+    gameBoard.appendChild(outerCell);
+
+    for (let j = 0; j < practiseBoard[i].length; j++) {
+        let innerCell = document.createElement("div");
+        
+        innerCell.classList.add(["innerCell"]);
+        innerCell.setAttribute(`id`,`cell-${i}-${j}` ); 
+        innerCell.innerText = practiseBoard[i][j];
+
+        outerCell.appendChild(innerCell);
+    }
+  }
+}
+
+generateGameBoard();
+

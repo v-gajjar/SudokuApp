f615f394fef91e4620d9d26b437da2f4c4fc9baf
JoleneKearse <pharaohnutz@gmail.com>
2024-09-26 14:51:44 -0400

    fixes #9: create help dialog

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 "use strict";
 
+const helpDialog = document.getElementById("helpDialog")
+
 const practiseBoard = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  [1, 2, 3, 4, 5, 6, 7, 8, 9],
 ];
-    
+
 function generateGameBoard() {
   // Set height of gameBoard
   const gameBoard = document.getElementById("gameBoard");
@@ -21,28 +23,26 @@ function generateGameBoard() {
   gameBoard.innerHTML = "";
 
   for (let i = 0; i < practiseBoard.length; i++) {
-
     let outerCell = document.createElement("div");
     outerCell.classList.add(["outerCell"]);
-    outerCell.setAttribute(`id`,`cell-${i}` ); 
+    outerCell.setAttribute(`id`, `cell-${i}`);
     outerCell.classList.add(["border-2"]);
     outerCell.classList.add(["grid"]);
     outerCell.classList.add(["grid-cols-3"]);
-    
-    
+
     gameBoard.appendChild(outerCell);
 
     for (let j = 0; j < practiseBoard[i].length; j++) {
-        let innerCell = document.createElement("div");
-        
-        innerCell.classList.add(["innerCell"]);
-        innerCell.classList.add(["border"]);
-        innerCell.classList.add(["text-center"]);
-        innerCell.classList.add(["align-middle"]);
-        innerCell.setAttribute(`id`,`cell-${i}-${j}` ); 
-        innerCell.innerText = practiseBoard[i][j];
+      let innerCell = document.createElement("div");
 
-        outerCell.appendChild(innerCell);
+      innerCell.classList.add(["innerCell"]);
+      innerCell.classList.add(["border"]);
+      innerCell.classList.add(["text-center"]);
+      innerCell.classList.add(["align-middle"]);
+      innerCell.setAttribute(`id`, `cell-${i}-${j}`);
+      innerCell.innerText = practiseBoard[i][j];
+
+      outerCell.appendChild(innerCell);
     }
   }
 }
@@ -53,3 +53,12 @@ window.addEventListener("resize", () => {
   generateGameBoard();
 });
 
+document.getElementById("help").addEventListener("click", () => {
+  helpDialog.classList.remove("hidden");
+  helpDialog.classList.add("block");
+
+  helpDialog.addEventListener("click", () => {
+    helpDialog.classList.remove("block");
+    helpDialog.classList.add("hidden");
+  });
+});

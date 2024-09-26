f6e496a9c65eec78f9984933199d4b1635bce863
vgajjar <vgajjar22@gmail.com>
2024-09-26 14:59:32 +0100

    First attempt making grid look like sudoku grid

--- main.js
+++ main.js
@@ -18,19 +18,27 @@ const practiseBoard = [
 ];
     
 function generateGameBoard() {
+  gameBoard.innerHTML = "";
+
   for (let i = 0; i < practiseBoard.length; i++) {
 
     let outerCell = document.createElement("div");
     outerCell.classList.add(["outerCell"]);
     outerCell.setAttribute(`id`,`cell-${i}` ); 
     outerCell.classList.add(["border-2"]);
-
+    outerCell.classList.add(["grid"]);
+    outerCell.classList.add(["grid-cols-3"]);
+    
+    
     gameBoard.appendChild(outerCell);
 
     for (let j = 0; j < practiseBoard[i].length; j++) {
         let innerCell = document.createElement("div");
         
         innerCell.classList.add(["innerCell"]);
+        innerCell.classList.add(["border"]);
+        innerCell.classList.add(["text-center"]);
+        innerCell.classList.add(["align-middle"]);
         innerCell.setAttribute(`id`,`cell-${i}-${j}` ); 
         innerCell.innerText = practiseBoard[i][j];
 
